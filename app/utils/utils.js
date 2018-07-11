037fda939a651d285752bc5a52c63fd39a55368b
zhujingxing <zhu.jx@andui.com>
2018-07-11 11:43:11 +0800

    code review

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -2,19 +2,16 @@ import EOS from 'eosjs';
 
 const localChainId =
   'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
-// const chainId =
-//   '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca';
 
 const onLineAddress = 'https://tool.eoscannon.io/';
 
 const getTransactionHeadersFromJsonInfo = jsonInfo => {
   const { refBlockNum, refBlockPrefix, expiration } = JSON.parse(jsonInfo);
-  const transactionHeaders = {
+  return {
     expiration,
     ref_block_num: refBlockNum,
     ref_block_prefix: refBlockPrefix,
   };
-  return transactionHeaders;
 };
 
 const getChainIdFromJsonInfoOrConfig = jsonInfo => {
@@ -27,13 +24,12 @@ const getEos = values => {
   const { keyProvider, jsonInfo } = values;
   const transactionHeaders = getTransactionHeadersFromJsonInfo(jsonInfo);
   const chainId = getChainIdFromJsonInfoOrConfig(jsonInfo);
-  const eos = EOS({
+  return EOS({
     httpEndpoint: null,
     chainId,
     keyProvider,
     transactionHeaders,
   });
-  return eos;
 };
 export {
   onLineAddress,
